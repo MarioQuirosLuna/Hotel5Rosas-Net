beb86453f2037ce40f34899777ccf043e05890e2
Joseph Moya <josephmoyac0395@gmail.com>
2023-04-03 17:08:21 -0600

    Entidad

---
+++ Entities_Hotel_5_Rosas/Entity_PaginaHome.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities_Hotel_5_Rosas
+{
+    public class Entity_PaginaHome
+    {
+        public string Nombre { get; set; }
+        public string Titulo { get; set; }
+        public string Informacion { get; set; }
+        public string Imagen { get; set; }
+
+    }
+}
