04ee3e1cd7731a63bb9c003313c2502e87bc5867
MarioQuirosL <marioql.dev@gmail.com>
2023-04-03 15:38:02 -0600

    cors

--- Hotel_5_Rosas_Proyect/Startup.cs
+++ Hotel_5_Rosas_Proyect/Startup.cs
@@ -96,6 +96,8 @@ namespace Hotel_5_Rosas_Proyect
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseCors();
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
