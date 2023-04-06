d36ce0efee73949aca5b76170a23c14ec5f8b873
MarioQuirosL <marioql.dev@gmail.com>
2023-04-05 18:52:39 -0600

    Obtener galleria de l pagina sobre nosotros

--- Hotel_5_Rosas_Proyect/Controllers/Entity_GaleriaController.cs
+++ Hotel_5_Rosas_Proyect/Controllers/Entity_GaleriaController.cs
@@ -8,6 +8,7 @@ using Microsoft.EntityFrameworkCore;
 using Entities_Hotel_5_Rosas;
 using Hotel_5_Rosas_Proyect.Data;
 using System.Web.Http.Cors;
+using Microsoft.Data.SqlClient;
 
 namespace Hotel_5_Rosas_Proyect.Controllers
 {
@@ -30,6 +31,29 @@ namespace Hotel_5_Rosas_Proyect.Controllers
         public async Task<IEnumerable<Entities_Hotel_5_Rosas.Entity_Galeria>> GetGaleries()
         {
             return await _context.Set<Entities_Hotel_5_Rosas.Entity_Galeria>().ToListAsync();
+        }
+
+        // GET: api/Entity_Galeria/getGalleryAboutUs
+        [HttpGet]
+        public List<Entities_Hotel_5_Rosas.Entity_Imagen> getGalleryAboutUs()
+        {
+            SqlConnection conexion = (SqlConnection)_context.Database.GetDbConnection();
+            SqlCommand cmd = conexion.CreateCommand();
+            conexion.Open();
+            cmd.CommandType = System.Data.CommandType.StoredProcedure;
+            cmd.CommandText = "SP_Obtener_Galeria_Sobre_Nosotros";
+
+            SqlDataReader reader = cmd.ExecuteReader();
+            List<Entity_Imagen> tipos = new List<Entity_Imagen>();
+            while (reader.Read())
+            {
+                Entity_Imagen tipo = new Entity_Imagen();
+                tipo.Imagen = (string)reader["Imagen"];
+                tipos.Add(tipo);
+            }
+            conexion.Close();
+
+            return tipos;
         }
 
         private bool Entity_GaleriaExists(int id)
