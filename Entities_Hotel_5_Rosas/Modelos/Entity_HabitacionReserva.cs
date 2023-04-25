3311cd5edeb7a56a9a1c47868f08c446a33eb88b
MarioQuirosLuna <marioql.dev@gmail.com>
2023-04-25 12:06:53 -0600

    Reservation

---
+++ Entities_Hotel_5_Rosas/Modelos/Entity_HabitacionReserva.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities_Hotel_5_Rosas.Modelos
+{
+    public class Entity_HabitacionReserva
+    {
+        public int PK_habitacion { get; set; }
+        public string Tipo_Habitacion { get; set; }
+        public int Numero_Habitacion { get; set; }
+        public string Imagen { get; set; }
+        public string Descripcion { get; set; }
+        public decimal Tarifa { get; set; }
+        public float Oferta { get; set; }
+        public string Nombre_Temporada { get; set; }
+    }
+}
--- Hotel_5_Rosas_Proyect/Controllers/Entity_ReservaController.cs
+++ Hotel_5_Rosas_Proyect/Controllers/Entity_ReservaController.cs
@@ -8,6 +8,8 @@ using Microsoft.EntityFrameworkCore;
 using Entities_Hotel_5_Rosas;
 using Hotel_5_Rosas_Proyect.Data;
 using System.Web.Http.Cors;
+using Entities_Hotel_5_Rosas.Modelos;
+using Microsoft.Data.SqlClient;
 
 namespace Hotel_5_Rosas_Proyect.Controllers
 {
@@ -30,9 +32,41 @@ namespace Hotel_5_Rosas_Proyect.Controllers
             return await _context.Set<Entities_Hotel_5_Rosas.Entity_Reserva>().ToListAsync();
         }
 
+        //----------------------------Available Room-----------------------------
+        // GET: api/Entity_Reserva/GetAvalaibleRoom
+        [HttpGet]
+        public ActionResult<Entity_HabitacionReserva> GetAvalaibleRoom(DateTime beginDate, DateTime endDate, int tipeRoom) {
+            try {
+                SqlConnection conexion = (SqlConnection)_context.Database.GetDbConnection();
+                SqlCommand cmd = conexion.CreateCommand();
+                conexion.Open();
+                cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                cmd.CommandText = "$@\"EXEC SP_Habitacion_Disponible @param_Fecha_Inicio={beginDate}, @param_Fecha_Fin={endDate}, @param_Tipo_Habitacion={tipeRoom}\"";
 
+                SqlDataReader reader = cmd.ExecuteReader();
+                List<Entity_HabitacionReserva> response = new List<Entity_HabitacionReserva>();
+                while (reader.Read())
+                {
+                    Entity_HabitacionReserva room = new Entity_HabitacionReserva();
+                    room.PK_habitacion = (int)reader["PK_Habitacion"];
+                    room.Tipo_Habitacion = (string)reader["Nombre"];
+                    room.Numero_Habitacion = (int)reader["Numero_Habitacion"];
+                    room.Imagen = (string)reader["Imagen"];
+                    room.Descripcion = (string)reader["Descripcion"];
+                    room.Tarifa = (decimal)reader["Tarifa"];
+                    room.Oferta = (float)reader["Oferta"];
+                    room.Nombre_Temporada = (string)reader["Nombre"];
+                    response.Add(room);
+                }
+                conexion.Close();
 
-
+                return Ok(response);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
 
         //----------------------------DELETE-----------------------------
 
