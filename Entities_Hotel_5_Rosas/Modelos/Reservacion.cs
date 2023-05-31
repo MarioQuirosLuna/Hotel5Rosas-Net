8b548bf3534aa901b7bfe98fc3ae6154d9eb26c4
MarioQuirosL <marioql.dev@gmail.com>
2023-05-31 16:42:55 -0600

    fix

---
+++ Entities_Hotel_5_Rosas/Modelos/Reservacion.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities_Hotel_5_Rosas.Modelos
+{
+   
+    public class Reservacion
+    {
+
+        [Key]
+        public int PK_Reserva { get; set; }
+        public string Tipo_Habitacion { get; set; }
+        public string Nombre_Cliente { get; set; }
+        public string Apellidos_Cliente { get; set; }
+        public string Numero_Tarjeta { get; set; }
+        public string Correo { get; set; }
+
+        public DateTime Fecha_Transaccion { get; set; }
+        public DateTime Fecha_Inicio { get; set; }
+        public DateTime Fecha_Fin { get; set; }
+        public decimal Tarifa_Total { get; set; }
+
+    }
+}
